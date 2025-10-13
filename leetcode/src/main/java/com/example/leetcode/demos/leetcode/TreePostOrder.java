22c6af387d16c022ba045def5cf344baf5a806af
woyaobianqiang <1038039989@qq.com>
2025-10-13 10:18:45 +0800

    1

---
+++ leetcode/src/main/java/com/example/leetcode/demos/leetcode/TreePostOrder.java
@@ -0,0 +1,41 @@
+package com.example.leetcode.demos.leetcode;
+
+import com.example.leetcode.demos.leetcode.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class TreePostOrder {
+
+    public List<Integer> postorderTraversalIterative(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+        }
+
+        // 根 右 左 -> reverse
+        List<Integer> res = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+
+            // root
+            res.add(pop.val);
+
+            // 左
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+
+            // 右
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+        }
+
+        Collections.reverse(res);
+
+        return res;
+    }
+}
